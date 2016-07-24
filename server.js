3bce08578d946eb856ec1e4f54dcb6627d3d759d
justinelite <justin858@hotmail.com>
2016-07-25 08:12:43 +1000

    update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-var PORT = process.env.PORT || 3000
+var PORT = 3000
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
 })
